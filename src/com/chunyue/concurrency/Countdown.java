0a2bbecfa160f0ac13015aea741958b07e767624
Chunyue Ma <cma01@wesleyan.edu>
2021-07-14 11:05:51 -0400

    java concurrency: thread variables and race condition for instance variable

---
+++ src/com/chunyue/concurrency/Countdown.java
@@ -0,0 +1,41 @@
+package com.chunyue.concurrency;
+
+public class Countdown {
+    // this does not work with multithreading since it is stored on the heap
+    // which all the threads would have the access => race condition
+    // private int i;
+
+    public void doCountDown(){
+        String color;
+        switch(Thread.currentThread().getName()){
+            case "Thread1":
+                color = ThreadColor.ANSI_CYAN;
+                break;
+            case "Thread2":
+                color = ThreadColor.ANSI_BLUE;
+                break;
+            default:
+                color = ThreadColor.ANSI_RED;
+        }
+
+        // declaring the int i here will force the i to be stored on the stack
+        for (int i = 10; i>0; i--){
+            System.out.println(color + Thread.currentThread().getName() + ": i= " + i);
+        }
+    }
+}
+
+
+class CountdownThread extends Thread {
+
+
+    private Countdown threadCountdown;
+
+    public CountdownThread(Countdown countdown){
+        this.threadCountdown = countdown;
+    }
+
+    public void run(){
+        this.threadCountdown.doCountDown();
+    }
+}
--- src/com/chunyue/concurrency/Main.java
+++ src/com/chunyue/concurrency/Main.java
@@ -1,47 +1,55 @@
 package com.chunyue.concurrency;
 
-import static com.chunyue.concurrency.ThreadColor.*;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println(ANSI_PURPLE + "Hi from main thread!");
-        // create another thread
-        Thread anotherThread = new AnotherThread();
-        anotherThread.setName(" ==Another Thread== ");
-        // this runs the new thread we created: we can only start it once
-        anotherThread.start();
-        // anotherThread.run();
+        Countdown countdown = new Countdown();
+        CountdownThread t1 = new CountdownThread(countdown);
+        t1.setName("Thread1");
 
-        // this gives: java.lang.IllegalThreadStateException
-        // anotherThread.start();
+        CountdownThread t2 = new CountdownThread(countdown);
+        t2.setName("Thread2");
 
-        // anonymous class thread
-        new Thread() {
-            public void run(){
-                System.out.println(ANSI_GREEN + "Hello from anonymous class thread");
-            }
-        }.start();
+        t1.start();
+        t2.start();
 
-        Thread myRunnable = new Thread(new MyRunnable());
-
-        Thread myNewRunnable = new Thread(new MyRunnable(){
-            @Override
-            public void run() {
-                System.out.println(ANSI_CYAN + "Hello from anonymous runnable thread");
-                try {
-                    anotherThread.join(5000);
-                    System.out.println(ANSI_CYAN + "Another thread is terminated or timedout, I am awake to do more" +
-                            " stuff");
-                } catch (InterruptedException e){
-                    System.out.println(ANSI_CYAN + "Sorry I cannot wait!");
-                }
-
-            }
-        });
-
-        myNewRunnable.start();
-        myRunnable.start();
-        // anotherThread.interrupt();
-        System.out.println(ANSI_PURPLE + "Hello again from the main thread");
+//        System.out.println(ANSI_PURPLE + "Hi from main thread!");
+//        // create another thread
+//        Thread anotherThread = new AnotherThread();
+//        anotherThread.setName(" ==Another Thread== ");
+//        // this runs the new thread we created: we can only start it once
+//        anotherThread.start();
+//        // anotherThread.run();
+//
+//        // this gives: java.lang.IllegalThreadStateException
+//        // anotherThread.start();
+//
+//        // anonymous class thread
+//        new Thread() {
+//            public void run(){
+//                System.out.println(ANSI_GREEN + "Hello from anonymous class thread");
+//            }
+//        }.start();
+//
+//        Thread myRunnable = new Thread(new MyRunnable());
+//
+//        Thread myNewRunnable = new Thread(new MyRunnable(){
+//            @Override
+//            public void run() {
+//                System.out.println(ANSI_CYAN + "Hello from anonymous runnable thread");
+//                try {
+//                    anotherThread.join(5000);
+//                    System.out.println(ANSI_CYAN + "Another thread is terminated or timedout, I am awake to do more" +
+//                            " stuff");
+//                } catch (InterruptedException e){
+//                    System.out.println(ANSI_CYAN + "Sorry I cannot wait!");
+//                }
+//
+//            }
+//        });
+//
+//        myNewRunnable.start();
+//        myRunnable.start();
+//        // anotherThread.interrupt();
+//        System.out.println(ANSI_PURPLE + "Hello again from the main thread");
     }
 }
