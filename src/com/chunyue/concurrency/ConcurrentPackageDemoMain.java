b6fa67fe50d6932f2a95769e4b2aa6b94272414f
Chunyue Ma <cma01@wesleyan.edu>
2021-07-21 11:58:44 -0400

    java concurrency: use renentrant lock

--- src/com/chunyue/concurrency/ConcurrentPackageDemoMain.java
+++ src/com/chunyue/concurrency/ConcurrentPackageDemoMain.java
@@ -3,6 +3,7 @@ package com.chunyue.concurrency;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.locks.ReentrantLock;
 
 import static com.chunyue.concurrency.ConcurrentPackageDemoMain.EOF;
 
@@ -11,9 +12,10 @@ public class ConcurrentPackageDemoMain {
     public static void main(String[] args) {
         // ArrayList is not thread safe, will encounter thread interference
         List<String> buffer = new ArrayList<>();
-        MyProvider provider = new MyProvider(buffer, ThreadColor.ANSI_RED);
-        MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_BLUE);
-        MyConsumer consumer2 = new MyConsumer(buffer, ThreadColor.ANSI_GREEN);
+        ReentrantLock bufferLock = new ReentrantLock();
+        MyProvider provider = new MyProvider(buffer, ThreadColor.ANSI_RED, bufferLock);
+        MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_BLUE, bufferLock);
+        MyConsumer consumer2 = new MyConsumer(buffer, ThreadColor.ANSI_GREEN, bufferLock);
 
         new Thread(provider).start();
         new Thread(consumer1).start();
@@ -24,10 +26,12 @@ public class ConcurrentPackageDemoMain {
 class MyProvider implements Runnable {
     private List<String> buffer;
     private String color;
+    private ReentrantLock bufferLock;
 
-    public MyProvider(List<String> buffer, String color) {
+    public MyProvider(List<String> buffer, String color, ReentrantLock bufferLock) {
         this.buffer = buffer;
         this.color = color;
+        this.bufferLock = bufferLock;
     }
 
     public void run(){
@@ -37,47 +41,51 @@ class MyProvider implements Runnable {
         for (String num : nums){
             try {
                 System.out.println(color + "Adding... " + num);
-                // have to synchronize the buffer
-                synchronized (buffer) {
-                    buffer.add(num);
-                }
+                bufferLock.lock();
+                buffer.add(num);
+                bufferLock.unlock();
                 Thread.sleep(random.nextInt(1000));
             } catch (InterruptedException e) {
                 System.out.println("Producer interrupted");
             }
         }
         System.out.println(color + "Adding EOF and exiting...");
-        synchronized (buffer) {
-            buffer.add("EOF");
-        }
+        // acquire the lock
+        bufferLock.lock();
+        buffer.add("EOF");
+        // manually release the lock
+        bufferLock.unlock();
     }
 }
 
 class MyConsumer implements Runnable {
     private List<String> buffer;
     private String color;
-
-    public MyConsumer(List<String> buffer, String color) {
+    private ReentrantLock bufferLock;
+    public MyConsumer(List<String> buffer, String color, ReentrantLock bufferLock) {
         this.buffer = buffer;
         this.color = color;
+        this.bufferLock = bufferLock;
     }
 
     public void run() {
         while(true){
-            // everything in the critical section
-            synchronized (buffer){
-                if (buffer.isEmpty()){
-                    continue;
-                }
-                // do not remove the EOF since if it is removed
-                // all the other consumers will run indefinitely
-                if (buffer.get(0).equals(EOF)){
-                    System.out.println(color + "Exiting");
-                    break;
-                } else {
-                    System.out.println(color + "Removed " + buffer.remove(0));
-                }
+            // acquire the lock
+            bufferLock.lock();
+            if (buffer.isEmpty()){
+                bufferLock.unlock();
+                continue;
             }
+            // do not remove the EOF since if it is removed
+            // all the other consumers will run indefinitely
+            if (buffer.get(0).equals(EOF)){
+                System.out.println(color + "Exiting");
+                bufferLock.unlock();
+                break;
+            } else {
+                System.out.println(color + "Removed " + buffer.remove(0));
+            }
+            bufferLock.unlock();
         }
     }
 }
