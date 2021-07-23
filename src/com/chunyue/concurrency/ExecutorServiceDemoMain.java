ab2c24651aa90e3585da868ba5dfeb4b4d873f32
Chunyue Ma <cma01@wesleyan.edu>
2021-07-23 16:59:01 -0400

    java concurrency: executor service thread pool types discussion

--- src/com/chunyue/concurrency/ExecutorServiceDemoMain.java
+++ src/com/chunyue/concurrency/ExecutorServiceDemoMain.java
@@ -12,11 +12,19 @@ public class ExecutorServiceDemoMain {
         int coreCount = Runtime.getRuntime().availableProcessors();
         System.out.println("You have " + coreCount + " cores");
         ExecutorService service = Executors.newFixedThreadPool(10);
+        // ExecutorService service = Executors.newCachedThreadPool();
+        // ExecutorService service = Executors.newScheduledThreadPool(10);
+
 
         // Submit the tasks for execution
         // Within the service the tasks submitted uses a blocking queue, which is thread safe
         for (int i = 0; i < 100; i++){
             service.execute(new Task());
+            //only available if we use the Executors.newScheduledThreadPool(10);
+            //service.schedule(new Task(), 10, SECONDS);
+            //service.scheduleAtFixedRate(new Task(), 10, SECONDS);
+            //service.scheduleWithFixedDelay(new Task(), 10, SECONDS);
+
         }
         System.out.println("Thread Name: " + Thread.currentThread().getName());
     }
@@ -32,3 +40,12 @@ public class ExecutorServiceDemoMain {
 // I/O intensive operation: threads probably spend most of the time waiting,
 //        you can have large pool so as increase the chance of threads running
 // CPU intensive operations: # of pool size == # of core count of the system
+
+// TYPE OF POOLS
+// Fixed size pool: fixed number of threads getting tasks from the queue
+// No fixed size pool: only use synchronous queue, which holds the task, look for free thread to execute, if there
+// are threads available, the thread will be used, if not, a new thread is created / also kill threads if it stays
+// idle for 60s
+// Scheduled pool: execute something after certain delays, holds a delay queue. The tasks are stored based on the delay
+// Single Thread Pool: only have one thread in the pool, used to ensure that the tasks on the blocking queue are ran
+// sequentially
