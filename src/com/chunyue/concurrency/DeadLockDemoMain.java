77a323b9f225775ae586c3ea0e2a6502c9500406
Chunyue Ma <cma01@wesleyan.edu>
2021-07-24 20:56:31 -0400

    java concurrency: simple deadlock demo

---
+++ src/com/chunyue/concurrency/DeadLockDemoMain.java
@@ -0,0 +1,53 @@
+package com.chunyue.concurrency;
+
+public class DeadLockDemoMain {
+
+    public static Object lock1 = new Object();
+    public static Object lock2 = new Object();
+
+    public static void main(String[] args) {
+        new Thread1().start();
+        new Thread2().start();
+        // thread 1 and thread 2 are blocking each other
+    }
+
+    private static class Thread1 extends Thread {
+        public void run(){
+            synchronized (lock1){
+                System.out.println("Thread 1: has lock1");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e){
+
+                }
+                System.out.println("Thread 1: waiting for lock2");
+                synchronized (lock2){
+                    System.out.println("Thread 1: has lock1 and lock2");
+                }
+                System.out.println("Thread 1: released lock 2");
+            }
+            System.out.println("Thread 1: released lock 1. Exiting...");
+        }
+    }
+
+    private static class Thread2 extends Thread {
+        public void run(){
+            synchronized (lock2){
+                System.out.println("Thread 2: has lock2");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e){
+
+                }
+                System.out.println("Thread 2: waiting for lock1");
+                synchronized (lock1){
+                    System.out.println("Thread 2: has lock2 and lock1");
+                }
+                System.out.println("Thread 2: released lock 1");
+            }
+            System.out.println("Thread 2: released lock 2. Exiting...");
+        }
+    }
+}
+
+
