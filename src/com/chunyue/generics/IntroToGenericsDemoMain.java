8973f3c4e483699d1ae2530d18dba6952df01b4c
Chunyue Ma <cma01@wesleyan.edu>
2021-07-25 17:17:45 -0400

    java generics: bounded type parameters

--- src/com/chunyue/generics/IntroToGenericsDemoMain.java
+++ src/com/chunyue/generics/IntroToGenericsDemoMain.java
@@ -5,9 +5,13 @@ public class IntroToGenericsDemoMain {
         // Generics introduced strong type checking
         // Eliminate explicit casting
         // Enables design of generic algorithm
-        Glass<Juice> juiceGlass = new Glass<>();
-        Juice juice = new Juice();
+
+        JuiceGlass<OrangeJuice> juiceGlass = new JuiceGlass<>();
+        OrangeJuice juice = new OrangeJuice();
         juiceGlass.liquid = juice;
+
+        // JuiceGlass<Water> will not be valid since water class
+        // does not extends Juice and implements Liquid
 
         Glass<Water> waterGlass = new Glass<>();
         Water water = new Water();
@@ -18,9 +22,22 @@ public class IntroToGenericsDemoMain {
 
 }
 
-class Glass<T>{
+interface Liquid{}
+class Glass<T extends Liquid>{
     public T liquid;
 }
-class Juice{}
-class Water{}
 
+// bound the types allowed to instantiate the glass with to only Juice
+class JuiceGlass<T extends Juice & Liquid>{
+    public T liquid;
+}
+
+class Juice {}
+class OrangeJuice extends Juice implements Liquid{};
+class AppleJuice extends Juice implements Liquid{}
+class Water implements Liquid{}
+
+// since cake and oil do not extend juice and implement liquid
+// they cannot be used as a valid type to instantiate glass or juiceglass
+class Cake{}
+class Oil{}
