9b73ee3363c878682ea016b809ff91b2936b6c83
Chunyue Ma <cma01@wesleyan.edu>
2021-07-26 12:50:51 -0400

    java collections: implement and use comparator interface #2

--- src/com/chunyue/collections/ArrayListDemoMain.java
+++ src/com/chunyue/collections/ArrayListDemoMain.java
@@ -2,13 +2,14 @@ package com.chunyue.collections;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class ArrayListDemoMain {
     public static void main(String[] args) {
         // List interface extends the Collection interface
         List<Integer> values = new ArrayList<>();
-        values.add(33);
+        values.add(36);
         values.add(44);
         values.add(55);
         // Not possible with collection interface
@@ -20,6 +21,32 @@ public class ArrayListDemoMain {
         System.out.println(values);
         Collections.reverse(values);
         System.out.println(values);
+
+        // Use the comparator we defined to sort the list
+        // The list will be sorted with the magnitude of the last digit
+        Comparator<Integer> com = new LastDigitComparator();
+        Collections.sort(values, com);
+        System.out.println(values);
+
+        // Define annnoynous comparator
+        Comparator<Integer> com2 = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (o1 % 100 > o2 % 100){
+                    return 1;
+                }
+                return -1;
+            }
+        };
+        Collections.sort(values, com2);
+        System.out.println(values);
+
+        // Define comparator through lambda comparison
+        Collections.sort(values, (o1, o2) ->{
+            return o1 * -1 > o2 * -1 ? 1 : -1;
+        });
+        System.out.println(values);
+
 
         // Stream API
         values.forEach(System.out::println);
---
+++ src/com/chunyue/collections/LastDigitComparator.java
@@ -0,0 +1,13 @@
+package com.chunyue.collections;
+
+import java.util.Comparator;
+
+// create this comparator class which implements the comparator interface
+public class LastDigitComparator implements Comparator<Integer> {
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        if (o1 % 10 > o2 % 10){
+            return 1;
+        } return -1;
+    }
+}
