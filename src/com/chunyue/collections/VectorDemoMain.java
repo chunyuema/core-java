583a773233035ba79297cd4915c9cfe1a3bc6254
Chunyue Ma <cma01@wesleyan.edu>
2021-07-26 09:58:56 -0400

    java colection: vector compare to arraylist #1

--- src/com/chunyue/collections/VectorDemoMain.java
+++ src/com/chunyue/collections/VectorDemoMain.java
@@ -1,5 +1,6 @@
 package com.chunyue.collections;
 
+import java.util.ArrayList;
 import java.util.Vector;
 
 public class VectorDemoMain {
@@ -34,5 +35,17 @@ public class VectorDemoMain {
         // does not automatically reduce the capacity
         System.out.println(v.capacity());
         System.out.println(v);
+
+        ArrayList<Integer> v2 = new ArrayList<>();
+        System.out.println(v2.size());
+        v2.add(2);
+        System.out.println(v2.size());
+        v2.add(2);
+        System.out.println(v2.size());
+        // ArrayList increase the capacity more slowly
+        // Only about 50% everytime it increases
+
+        // ArrayList is not thread safe
+        // Vector is synchronized (thread safe)
     }
 }
