f33767e7f3b91a6c97e843305c3ca03aa31acaa4
Chunyue Ma <cma01@wesleyan.edu>
2021-07-27 09:57:29 -0400

    java collection: custom type comparator #2

--- src/com/chunyue/collections/comparator/CustomTypeComparatorDemoMain.java
+++ src/com/chunyue/collections/comparator/CustomTypeComparatorDemoMain.java
@@ -1,2 +1,48 @@
-package com.chunyue.collections.comparator;public class CustomTypeComparatorDemoMain {
+package com.chunyue.collections.comparator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CustomTypeComparatorDemoMain {
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, 55));
+        students.add(new Student(2, 88));
+        students.add(new Student(3, 45));
+        students.add(new Student(4, 67));
+        students.add(new Student(5, 78));
+
+        for(Student s: students){
+            System.out.println(s);
+        }
+
+        // sort using a customized comparator
+        Collections.sort(students, (s1, s2)->{
+            return s1.mark > s2.mark ? -1 : s1.mark < s2.mark ? 1 : 0;
+        });
+
+        for(Student s: students){
+            System.out.println(s);
+        }
+
+    }
+}
+
+class Student{
+    int rollno;
+    int mark;
+
+    public Student(int rollno, int mark) {
+        this.rollno = rollno;
+        this.mark = mark;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "rollno=" + rollno +
+                ", mark=" + mark +
+                '}';
+    }
 }
