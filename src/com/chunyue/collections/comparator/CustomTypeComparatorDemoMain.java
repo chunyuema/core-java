ad1c18cc76abc603f95164c108955ee0d4755e15
Chunyue Ma <cma01@wesleyan.edu>
2021-07-27 10:04:55 -0400

    java collection: comparable interface #2

--- src/com/chunyue/collections/comparator/CustomTypeComparatorDemoMain.java
+++ src/com/chunyue/collections/comparator/CustomTypeComparatorDemoMain.java
@@ -26,9 +26,22 @@ public class CustomTypeComparatorDemoMain {
             System.out.println(s);
         }
 
+
+        List<StudentNew> newStudents = new ArrayList<>();
+        newStudents.add(new StudentNew(1, 55));
+        newStudents.add(new StudentNew(2, 88));
+        newStudents.add(new StudentNew(3, 45));
+        newStudents.add(new StudentNew(4, 67));
+        newStudents.add(new StudentNew(5, 78));
+        Collections.sort(newStudents);
+        // This works since studentnew class implements the comparable interface
+        for (StudentNew s: newStudents){
+            System.out.println(s);
+        }
     }
 }
 
+// The student class does not implement comparable interface
 class Student{
     int rollno;
     int mark;
@@ -44,5 +57,29 @@ class Student{
                 "rollno=" + rollno +
                 ", mark=" + mark +
                 '}';
+    }
+}
+
+// The new student class will implement comparable interface
+class StudentNew implements Comparable<StudentNew>{
+    int rollno;
+    int mark;
+
+    public StudentNew(int rollno, int mark) {
+        this.rollno = rollno;
+        this.mark = mark;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentNew{" +
+                "rollno=" + rollno +
+                ", mark=" + mark +
+                '}';
+    }
+
+    @Override
+    public int compareTo(StudentNew o) {
+        return this.mark > o.mark ? -1 : this.mark < o.mark ? 1: 0;
     }
 }
