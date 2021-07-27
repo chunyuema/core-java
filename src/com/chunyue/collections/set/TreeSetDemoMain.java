5971594f34be5553c91751109cae4573f091e683
Chunyue Ma <cma01@wesleyan.edu>
2021-07-27 09:45:50 -0400

    java collection: treeset demo #3

--- src/com/chunyue/collections/set/TreeSetDemoMain.java
+++ src/com/chunyue/collections/set/TreeSetDemoMain.java
@@ -1,2 +1,18 @@
-package com.chunyue.collections.set;public class TreeSetDemoMain {
+package com.chunyue.collections.set;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSetDemoMain {
+    // behaves like a simple set with the exception that it stores elements in a sorted format.
+    public static void main(String[] args) {
+        Set<String> values = new TreeSet<>();
+        values.add("grape");
+        values.add("pear");
+        values.add("apple");
+        // TreeSet will impose on some sort of ordering
+        System.out.println(values);
+        values.remove("grape");
+        System.out.println(values);
+    }
 }
