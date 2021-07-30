9a431944d81bb3c85f3437a0b0280391a5479ec1
Chunyue Ma <cma01@wesleyan.edu>
2021-07-29 22:34:40 -0400

    java lambda: quick lambda expression to create comparator

---
+++ src/com/chunyue/lamdaexpression/LambdaExpressionComparatorDemoMain.java
@@ -0,0 +1,65 @@
+package com.chunyue.lamdaexpression;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class LambdaExpressionComparatorDemoMain {
+    public static void main(String[] args) {
+        Employee chunyue = new Employee("Chunyue Ma", 22);
+        Employee jack = new Employee("Jack Ma", 42);
+        Employee alex = new Employee("Alex Ma", 10);
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(chunyue);
+        employeeList.add(jack);
+        employeeList.add(alex);
+        // traditional method of defining the comparator for sorting
+        Collections.sort(employeeList, new Comparator<Employee>() {
+            @Override
+            public int compare(Employee o1, Employee o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+        System.out.println(employeeList);
+        Employee bobby = new Employee("Bobby Ma", 10);
+        employeeList.add(bobby);
+        // use a lambda expression for quick creation of comparator
+        Collections.sort(employeeList, (o1, o2) -> o1.getName().compareTo(o2.getName()));
+        System.out.println(employeeList);
+    }
+}
+
+class Employee {
+    private String name;
+    private int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
--- src/com/chunyue/lamdaexpression/LambdaExpressionDemoMain.java
+++ src/com/chunyue/lamdaexpression/LambdaExpressionDemoMain.java
@@ -4,10 +4,14 @@ public class LambdaExpressionDemoMain {
     public static void main(String[] args) {
         // quick thread creation with lambda expression
         // lambda match the run() function into the lambda expression below
-        new Thread(()-> {
+        new Thread(() -> {
             System.out.println("Printing from Runnable");
             System.out.println("This is line 2");
             System.out.println("This is line 3");
         }).start();
+
+
     }
 }
+
+
