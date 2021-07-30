6ba7f100e0f635ee53666d13d37b645338fc2ef9
Chunyue Ma <cma01@wesleyan.edu>
2021-07-29 22:17:43 -0400

    java lambda: quick lambda expression demo with thread creation

---
+++ src/com/chunyue/lamdaexpression/LambdaExpressionDemoMain.java
@@ -0,0 +1,13 @@
+package com.chunyue.lamdaexpression;
+
+public class LambdaExpressionDemoMain {
+    public static void main(String[] args) {
+        // quick thread creation with lambda expression
+        // lambda match the run() function into the lambda expression below
+        new Thread(()-> {
+            System.out.println("Printing from Runnable");
+            System.out.println("This is line 2");
+            System.out.println("This is line 3");
+        }).start();
+    }
+}
