bd9902478b2fbc33e87f973d53b2b9e627021901
Chunyue Ma <cma01@wesleyan.edu>
2021-08-03 09:42:13 -0400

    add elements to linked list #8

--- src/com/chunyue/collection/list/LinkedListDemoMain.java
+++ src/com/chunyue/collection/list/LinkedListDemoMain.java
@@ -5,10 +5,33 @@ import java.util.LinkedList;
 
 public class LinkedListDemoMain {
     public static void main(String[] args) {
+        addToLinkedList();
+    }
+
+    public static void addToLinkedList(){
         LinkedList<String> values = new LinkedList<>();
+        // ============================================
         // adding items into linked list
         values.add("Chunyue");
         values.add("Bobby");
+        values.add(1, "Alex");
+
+        LinkedList<String> values2 = new LinkedList<>();
+        values2.add("John");
+        values2.add("Sam");
+
+        values.addAll(values2);
+
+        LinkedList<String> values3 = new LinkedList<>();
+        values3.add("Jane");
+        values3.add("Mariam");
+
+        values.addAll(1, values3);
+
+        values.addFirst("Start");
+        values.addLast("End");
+
+        // =============================================
         // iterating through linked list
         Iterator<String> iterator = values.iterator();
         while(iterator.hasNext()){
