18486027a9a3485897a507865f126c7fb38f8188
Chunyue Ma <cma01@wesleyan.edu>
2021-08-04 10:25:45 -0400

    linkedHashSet add and remove methods #8

---
+++ src/com/chunyue/collection/set/LinkedHashSetDemoMain.java
@@ -0,0 +1,34 @@
+package com.chunyue.collection.set;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class LinkedHashSetDemoMain {
+    public static void main(String[] args) {
+        Set<Integer> values = new LinkedHashSet<>();
+        System.out.println("Adding 6 for the first time: " + values.add(6) + ": " + values);
+        // Does not support duplicate values
+        System.out.println("Adding 6 for the second time: " + values.add(6) + ": " + values);
+
+        values.add(67);
+        values.add(79);
+        values.add(54);
+        values.add(90);
+        // Use linked list, there fore the insertion order is preserved
+        System.out.println("values = " + values);
+
+        Set<Integer> values2 = new HashSet<>();
+        values2.add(67);
+        values2.add(54);
+        System.out.println("values2 =  " + values2);
+        values.removeAll(values2);
+        System.out.println("Removing values2 from values: " + values);
+
+        values.removeIf(num -> num % 3 == 0);
+        System.out.println("Removing anything that is divisible by 3:  " + values);
+
+        values.clear();
+        System.out.println("Removing all from values" + values);
+    }
+}
