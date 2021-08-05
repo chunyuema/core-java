de4162771fa230a802c0bedf5bd068e5c52b14ab
Chunyue Ma <cma01@wesleyan.edu>
2021-08-05 10:11:01 -0400

    reverse each word and capitalize the first letter #9

--- src/com/chunyue/string/StringFAQDemoMain.java
+++ src/com/chunyue/string/StringFAQDemoMain.java
@@ -2,9 +2,11 @@ package com.chunyue.string;
 
 public class StringFAQDemoMain {
     public static void main(String[] args) {
-        reverseStringUsingStringBuilder("Hello World");
-        reverseStringUsingIteration("Hello You!");
-        checkPalindrome("abba");
+        //reverseStringUsingStringBuilder("hello my love");
+        // reverseStringUsingIteration("Hello You!");
+        // checkPalindrome("abba");
+        // System.out.println("Capitalizing hello my love: " + capitalize("hello my love"));
+        System.out.println("Reveser each word in hello my love: " + reverseEachWord("hello my love"));
     }
 
     public static String reverseStringUsingStringBuilder(String s){
@@ -32,5 +34,24 @@ public class StringFAQDemoMain {
             return true;
         }
         return false;
+    }
+
+    public static String capitalize(String s){
+        String[] words = s.split(" ");
+        String newString = "";
+        for (String word: words){
+            newString += word.substring(0,1).toUpperCase() + word.substring(1)+" ";
+        }
+        return newString.trim();
+    }
+
+    public static String reverseEachWord(String s){
+        String[] words = s.split(" ");
+        String newString = "";
+        for (String word: words){
+            StringBuilder sb = new StringBuilder(word);
+            newString += sb.reverse().toString() + " ";
+        }
+        return newString.trim();
     }
 }
