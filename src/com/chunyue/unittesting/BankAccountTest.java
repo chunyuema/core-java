269bff66a2bfea836e6b1bfd14b6569061ca0d2e
Chunyue Ma <cma01@wesleyan.edu>
2021-08-05 17:54:21 -0400

    set up junit5 unittesting with initial fail message #6

---
+++ src/com/chunyue/unittesting/BankAccountTest.java
@@ -0,0 +1,21 @@
+package com.chunyue.unittesting;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAccountTest {
+
+    @org.junit.jupiter.api.Test
+    void deposit() {
+        fail("Test is not implemented");
+    }
+
+    @org.junit.jupiter.api.Test
+    void withdraw() {
+        fail("Test is not implemented");
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBalance() {
+        fail("Test is not implemented");
+    }
+}
---
+++ src/com/chunyue/unittesting/JUnitDemoMain.java
@@ -0,0 +1,7 @@
+package com.chunyue.unittesting;
+
+public class JUnitDemoMain {
+    public static void main(String[] args) {
+        System.out.println("Application is running~");
+    }
+}
