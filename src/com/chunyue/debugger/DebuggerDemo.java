e3504763e2d912928cec18e60f2ce876814b65b5
Chunyue Ma <cma01@wesleyan.edu>
2021-08-05 17:38:38 -0400

    debugger demo code #6

--- src/com/chunyue/debugger/DebuggerDemo.java
+++ src/com/chunyue/debugger/DebuggerDemo.java
@@ -1,2 +1,12 @@
-package com.chunyue.debugger;public class DebuggerDemo {
+package com.chunyue.debugger;
+
+public class DebuggerDemo {
+    public static void main(String[] args) {
+        StringUtilities utils = new StringUtilities();
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() < 10){
+            utils.addChar(sb, 'a');
+        }
+        System.out.println(sb);
+    }
 }
--- src/com/chunyue/debugger/StringUtilities.java
+++ src/com/chunyue/debugger/StringUtilities.java
@@ -1,2 +1,11 @@
-package com.chunyue.debugger;public class StringUtilities {
+package com.chunyue.debugger;
+
+public class StringUtilities {
+    private StringBuilder stringBuilder = new StringBuilder();
+    private int charsAdded = 0;
+
+    public void addChar(StringBuilder stringBuilder, char c){
+        stringBuilder.append(c);
+        charsAdded++;
+    }
 }
