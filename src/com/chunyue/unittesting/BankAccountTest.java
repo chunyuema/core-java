821965dbf86368e86f1829fded698e13f1a21725
Chunyue Ma <cma01@wesleyan.edu>
2021-08-05 23:26:49 -0400

    junit testing with asserts #6

--- src/com/chunyue/unittesting/BankAccountTest.java
+++ src/com/chunyue/unittesting/BankAccountTest.java
@@ -3,19 +3,38 @@ package com.chunyue.unittesting;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BankAccountTest {
-
+    // needs to be annotated
     @org.junit.jupiter.api.Test
     void deposit() {
-        fail("Test is not implemented");
+        // return void
+        // a test should be self contained
+        BankAccount account = new BankAccount("Tim", "Ma", 1000.00);
+        double balance = account.deposit(200.00, true);
+        // the third parameter is the delta that is tolerated for error margin
+        assertEquals(1200, balance, 0);
     }
 
     @org.junit.jupiter.api.Test
-    void withdraw() {
-        fail("Test is not implemented");
+    void getBalance_withdraw() {
+        BankAccount account = new BankAccount("Tim", "Ma", 1000.00);
+        account.withdraw(200.00, true);
+        // the third parameter is the delta that is tolerated for error margin
+        assertEquals(800, account.getBalance(), 0);
     }
 
     @org.junit.jupiter.api.Test
-    void getBalance() {
-        fail("Test is not implemented");
+    void getBalance_deposit() {
+        // a test should be self contained
+        BankAccount account = new BankAccount("Tim", "Ma", 1000.00);
+        account.deposit(200.00, true);
+        // the third parameter is the delta that is tolerated for error margin
+        assertEquals(1200, account.getBalance(), 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    void dummyTest(){
+        // use assertEqauls to compare expected with the actual
+        assertEquals(21, 21);
+        assertEquals(20, 20);
     }
 }
