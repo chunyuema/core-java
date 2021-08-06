7f791e3bae4c00cce4ad2d4e33033b84da32d77d
Chunyue Ma <cma01@wesleyan.edu>
2021-08-06 11:26:05 -0400

    multiple catch blocks from specific to non-specific #7

--- src/com/chunyue/exceptions/TryCatchDemoMain.java
+++ src/com/chunyue/exceptions/TryCatchDemoMain.java
@@ -5,6 +5,8 @@ public class TryCatchDemoMain {
         // tryCatchException();
         // tryCatchArithmeticException();
         // tryCatchWrongException();
+        // multiCatchExceptionn();
+        // multiCatchExceptionWrongSequence();
     }
 
     public static void tryCatchException(){
@@ -32,5 +34,36 @@ public class TryCatchDemoMain {
             System.out.println("Exception occurred: " + e);
         }
         System.out.println("The rest of the code will proceed");
+    }
+
+    public static void multiCatchExceptionn(){
+        try {
+            int[] a = {1, 2, 4, 5};
+            System.out.println(a[2]);
+            System.out.println(a[6]);
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Array out of bounds!");
+        } catch (ArithmeticException e){
+            System.out.println("Arithmetic exceptions: " + e);
+        } catch (Exception e){
+            System.out.println("Parent Exception is handling this");
+        }
+        System.out.println("The rest of the code....");
+    }
+
+    // this one will not compile
+    public static void multiCatchExceptionWrongSequence(){
+        try {
+            int[] a = {1, 2, 4, 5};
+            System.out.println(a[2]);
+            System.out.println(a[6]);
+        } catch (Exception e){
+            System.out.println("Parent exception handling this!");}
+//        } catch (ArithmeticException e){
+//            System.out.println("Arithmetic exceptions: " + e);
+//        } catch (ArrayIndexOutOfBoundsException e){
+//            System.out.println("Array index out of bounds");
+//        }
+//        System.out.println("The rest of the code....");
     }
 }
