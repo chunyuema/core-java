dacbd430a6cd7e2d58173458841cfc21f8ca14e1
Chunyue Ma <cma01@wesleyan.edu>
2021-08-09 11:07:38 -0400

    annotation of code to run before/after all/each test #6

--- src/com/chunyue/unittesting/BankAccountTest.java
+++ src/com/chunyue/unittesting/BankAccountTest.java
@@ -3,20 +3,54 @@ package com.chunyue.unittesting;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BankAccountTest {
+
+    private BankAccount account;
+    private static int testCount;
+
+    @org.junit.jupiter.api.BeforeAll
+    public static void setupBeforeAllTest(){
+        System.out.println("This is executed before all the tests! Number of test ran: " + testCount++);
+    }
+
+    @org.junit.jupiter.api.AfterAll
+    public static void cleanUpAfterAllTest(){
+        System.out.println("This is executed after all the tests! Number of test ran: " + testCount++);
+    }
+
+    @org.junit.jupiter.api.BeforeEach // run before each test is completed
+    public void setupForEachTest(){
+        account = new BankAccount("Chunyue", "Ma", 1000.0, BankAccount.CHECKING);
+        System.out.println("Running the tests now");
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    public void cleanUpAfterEachTest(){
+        System.out.println("Count = " + testCount++);
+    }
+
     // needs to be annotated
     @org.junit.jupiter.api.Test
     void deposit() {
-        // return void
-        // a test should be self contained
-        BankAccount account = new BankAccount("Tim", "Ma", 1000.00, BankAccount.CHECKING);
         double balance = account.deposit(200.00, true);
         // the third parameter is the delta that is tolerated for error margin
         assertEquals(1200, balance, 0);
     }
 
     @org.junit.jupiter.api.Test
+    void withdraw_branch() throws Exception {
+        double balance = account.withdraw(600.0, true);
+        assertEquals(400, balance, 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    void withdraw_notbranch() throws Exception {
+        assertThrows(IllegalArgumentException.class, ()->{
+            double balance = account.withdraw(600.0, false);
+        });
+    }
+
+    @org.junit.jupiter.api.Test
     void getBalance_withdraw() {
-        BankAccount account = new BankAccount("Tim", "Ma", 1000.00, BankAccount.CHECKING);
         account.withdraw(200.00, true);
         // the third parameter is the delta that is tolerated for error margin
         assertEquals(800, account.getBalance(), 0);
@@ -25,7 +59,6 @@ class BankAccountTest {
     @org.junit.jupiter.api.Test
     void getBalance_deposit() {
         // a test should be self contained
-        BankAccount account = new BankAccount("Tim", "Ma", 1000.00, BankAccount.CHECKING);
         account.deposit(200.00, true);
         // the third parameter is the delta that is tolerated for error margin
         assertEquals(1200, account.getBalance(), 0);
@@ -33,7 +66,6 @@ class BankAccountTest {
 
     @org.junit.jupiter.api.Test
     public void isChecking_true(){
-        BankAccount account = new BankAccount("Chunyue", "Ma", 100, BankAccount.CHECKING);
         assertTrue(account.isChecking());
     }
 
