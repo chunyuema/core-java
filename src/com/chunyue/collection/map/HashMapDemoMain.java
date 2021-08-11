9907aa6d9e6eb91243fda718875650b9a8e406fa
Chunyue Ma <cma01@wesleyan.edu>
2021-08-11 12:16:33 -0400

    map entry example with hashmap #8

--- src/com/chunyue/collection/map/HashMapDemoMain.java
+++ src/com/chunyue/collection/map/HashMapDemoMain.java
@@ -5,35 +5,45 @@ import java.util.Map;
 import java.util.Set;
 
 public class HashMapDemoMain {
-    public static void main(String[] args) {
-        Map<String, String> map = new HashMap<>();
+
+    public static Map<String, String> map = new HashMap<>();
+
+    static {
         // There is no intrinsic ordering; similar to hash set
         map.put("first name", "chunyue");
         map.put("last name", "ma");
+    }
+    public static void main(String[] args) {
         System.out.println(map);
         System.out.println(map.get("first name"));
-        // return null, wont throw an error
-        System.out.println(map.get("first name yes"));
+        System.out.println(map.get("first name yes")); // return null, wont throw an error
+        // putForUpdate();
+        // keySeyDemo();
+        // mapEntryDemo();
+    }
 
-        // return an array of keys
+    public static void putForUpdate(){
+        System.out.println("==== use put to override the values====");
+        map.put("last name", "mama");
+        System.out.println(map);
+    }
+
+    public static void keySeyDemo(){
+        System.out.println("=====keySet() method demo =======");
         System.out.println((map.keySet()).getClass()); //class java.util.HashMap$KeySet
         Set<String> set = map.keySet();
         for (String key : set){
             System.out.println(key + ": " + map.get(key));
         }
 
-        // override what is already in the hashmap
-        map.put("last name", "mama");
-        System.out.println(map);
+    }
 
-        System.out.println(map.entrySet());
-
-        // HashTable V.S. HashMap
-        // hashtable is synchronized and slow, hashmap is not synchronized and fast
-        // hashmap is introduced in 1.2
-        // hashmap allows null keys but hashtable does not allow that
-
-        // LinkedHashMap: this maintains the insertion order of the elements
-        // TreeHashMap: this maintains the sorted version of the entries inserted
+    public static void mapEntryDemo(){
+        System.out.println("======Map.Entry Interface Demo=======");
+        Set<Map.Entry<String, String>> entrySet = map.entrySet();
+        System.out.println("The entry set is: " + entrySet);
+        for (Map.Entry<String, String> me: entrySet){
+            System.out.println(me);
+        }
     }
 }
