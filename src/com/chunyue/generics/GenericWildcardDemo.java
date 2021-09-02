64d948ed6b7751dd6839881a6c075828608ed7e9
Chunyue Ma <cma01@wesleyan.edu>
2021-09-02 17:42:45 -0400

    generic wild card demo #17

---
+++ src/com/chunyue/generics/GenericWildcardDemo.java
@@ -0,0 +1,27 @@
+package com.chunyue.generics;
+
+public class GenericWildcardDemo {
+
+    public static void main(String[] args) {
+        ShowMsg showMsg = new ShowMsg();
+
+        Generic<Integer> generic = new Generic<>();
+        generic.setFlag(25);
+        showMsg.showFlag(generic);
+
+        Generic<Number> generic1 = new Generic<>();
+        generic1.setFlag(34);
+        showMsg.showFlag(generic1);
+
+        Generic<String> generic2 = new Generic<>();
+        generic2.setFlag("Chunyue");
+        showMsg.showFlag(generic2);
+    }
+}
+
+class ShowMsg {
+    // support any types in the generic
+    public void showFlag(Generic<?> generic){
+        System.out.println(generic.getFlag());
+    }
+}
