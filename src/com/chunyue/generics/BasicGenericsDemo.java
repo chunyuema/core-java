b01ee5b13de6a428f51ddbf87b1b2e0a5cbacfc6
Chunyue Ma <cma01@wesleyan.edu>
2021-09-02 11:06:21 -0400

    basic generics demo #17

---
+++ src/com/chunyue/generics/BasicGenericsDemo.java
@@ -0,0 +1,27 @@
+package com.chunyue.generics;
+
+public class BasicGenericsDemo<T> {
+    public static void main(String[] args) {
+        Generic<String> generic = new Generic<>();
+        generic.setFlag("admin");
+        String flag = generic.getFlag();
+
+        Generic<Integer> generic1 = new Generic<>();
+        generic1.setFlag(1);
+        Integer flag1 = generic1.getFlag();
+
+        System.out.println(flag + "; " + flag1);
+    }
+}
+
+class Generic<T>{
+    private T flag;
+
+    public void setFlag(T flag){
+        this.flag = flag;
+    }
+
+    public T getFlag(){
+        return this.flag;
+    }
+}
