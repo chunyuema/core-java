d79552ebbe2c66ee26e72229771e207c8dbb2d48
Chunyue Ma <cma01@wesleyan.edu>
2021-09-02 17:52:01 -0400

    generic: set lower bound of wild card #17

--- src/com/chunyue/generics/GenericWildcardDemo.java
+++ src/com/chunyue/generics/GenericWildcardDemo.java
@@ -8,20 +8,26 @@ public class GenericWildcardDemo {
         Generic<Integer> generic = new Generic<>();
         generic.setFlag(25);
         showMsg.showFlag(generic);
+        // showMsg.showFlag2(generic); // lower bound restriction
 
         Generic<Number> generic1 = new Generic<>();
         generic1.setFlag(34);
         showMsg.showFlag(generic1);
+        showMsg.showFlag2(generic1);
 
         Generic<String> generic2 = new Generic<>();
         generic2.setFlag("Chunyue");
-        // showMsg.showFlag(generic2);
+        // showMsg.showFlag(generic2); // upper bound restriction
     }
 }
 
 class ShowMsg {
     // support any types in the generic
     public void showFlag(Generic<? extends Number> generic){
+        System.out.println(generic.getFlag());
+    }
+
+    public void showFlag2(Generic<? super Number> generic){
         System.out.println(generic.getFlag());
     }
 }
