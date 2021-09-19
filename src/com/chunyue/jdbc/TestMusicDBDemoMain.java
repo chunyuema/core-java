46f2ea8fb872938b6cd3fd220fe1ae5363ebc1f9
Chunyue Ma <cma01@wesleyan.edu>
2021-09-19 17:38:15 -0400

    query artist with song and query metadata #11

--- src/com/chunyue/jdbc/TestMusicDBDemoMain.java
+++ src/com/chunyue/jdbc/TestMusicDBDemoMain.java
@@ -1,9 +1,6 @@
 package com.chunyue.jdbc;
 
-import com.chunyue.jdbc.model.Artist;
 import com.chunyue.jdbc.model.MusicDataSource;
-
-import java.util.List;
 
 public class TestMusicDBDemoMain {
     public static void main(String[] args) {
@@ -13,25 +10,40 @@ public class TestMusicDBDemoMain {
             System.out.println("Cannot open music data source");
             return;
         }
-        List<Artist> artistList = dataSource.queryArtists(MusicDataSource.ORDER_BY_ASC);
-        if (artistList == null){
-            System.out.println("No artists");
-            return;
-        } else {
-            for (Artist artist : artistList){
-                System.out.println(artist);
-            }
-        }
 
-        List<String> albumsForArtist = dataSource.queryAlbumForArtist("Iron Maiden", MusicDataSource.ORDER_BY_ASC);
-        if (albumsForArtist != null){
-            System.out.println(albumsForArtist);
-            for (String album : albumsForArtist){
-                System.out.println(album);
-            }
-        } else {
-            System.out.println("No albums");
-        }
+        // query a list of artists
+//        List<Artist> artistList = dataSource.queryArtists(MusicDataSource.ORDER_BY_ASC);
+//        if (artistList == null){
+//            System.out.println("No artists");
+//            return;
+//        } else {
+//            for (Artist artist : artistList){
+//                System.out.println(artist);
+//            }
+//        }
+
+        // query the albums based on an artist
+//        List<String> albumsForArtist = dataSource.queryAlbumForArtist("Pink Floyd", MusicDataSource.ORDER_BY_DESC);
+//        if (albumsForArtist != null){
+//            for (String album : albumsForArtist){
+//                System.out.println(album);
+//            }
+//        } else {
+//            System.out.println("No albums found for this artist");
+//        }
+//
+//        List<SongArtist> songArtistList = dataSource.queryArtistForSong("Go Your Own Way",
+//                MusicDataSource.ORDER_BY_ASC);
+//        if (songArtistList == null){
+//            System.out.println("Could not find the artist for the song");
+//            return;
+//        }
+//        for (SongArtist artist : songArtistList){
+//            System.out.println(artist);
+//        }
+
+
+        dataSource.querySongsMetaData();
         dataSource.close();
     }
 }
--- src/com/chunyue/jdbc/model/MusicDataSource.java
+++ src/com/chunyue/jdbc/model/MusicDataSource.java
@@ -48,6 +48,19 @@ public class MusicDataSource {
     public static final String QUERY_ALBUMS_BY_ARTIST_SORT =
             " ORDER BY " + TABLE_ALBUMS + "." + COLUMN_ALBUM_NAME + " COLLATE NOCASE ";
 
+    public static final String QUERY_ARTIST_FOR_SONG_START =
+            "SELECT " + TABLE_ARTISTS + "." + COLUMN_ARTIST_NAME + ", " +
+                    TABLE_ALBUMS + "." + COLUMN_ALBUM_NAME + ", " +
+                    TABLE_SONGS + "." + COLUMN_SONG_TRACK + " FROM " + TABLE_SONGS +
+                    " INNER JOIN " + TABLE_ALBUMS + " ON " +
+                    TABLE_SONGS + "." + COLUMN_SONG_ALBUM + " = " + TABLE_ALBUMS + "." + COLUMN_ALBUM_ID +
+                    " INNER JOIN " + TABLE_ARTISTS + " ON " +
+                    TABLE_ALBUMS + "." + COLUMN_ALBUM_ARTIST + " = " + TABLE_ARTISTS + "." + COLUMN_ARTIST_ID +
+                    " WHERE " + TABLE_SONGS + "." + COLUMN_SONG_TITLE + " = \"";
+
+    public static final String QUERY_ARTIST_FOR_SONG_SORT =
+            " ORDER BY " + TABLE_ARTISTS + "." + COLUMN_ARTIST_NAME + ", " +
+                    TABLE_ALBUMS + "." + COLUMN_ALBUM_NAME + " COLLATE NOCASE ";
 
 
     private Connection connection;
@@ -127,6 +140,52 @@ public class MusicDataSource {
         } catch (SQLException e) {
             System.out.println("Query failed: " + e.getMessage());
             return null;
+        }
+    }
+
+    public List<SongArtist> queryArtistForSong(String songName, int sortOrder){
+        StringBuilder sb = new StringBuilder(QUERY_ARTIST_FOR_SONG_START);
+        sb.append(songName);
+        sb.append("\"");
+
+        if (sortOrder != ORDER_BY_NONE){
+            sb.append(QUERY_ARTIST_FOR_SONG_SORT);
+            if (sortOrder == ORDER_BY_DESC){
+                sb.append("DESC");
+            } else {
+                sb.append("ASC");
+            }
+        }
+
+        System.out.println("SQL Statement: " + sb.toString());
+
+        try (Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sb.toString())) {
+            List<SongArtist> songArtists = new ArrayList<>();
+            while (resultSet.next()){
+                SongArtist songArtist = new SongArtist();
+                songArtist.setArtistName(resultSet.getString(1));
+                songArtist.setAlbumName(resultSet.getString(2));
+                songArtist.setTrack(resultSet.getInt(3));
+            }
+             return songArtists;
+        } catch (SQLException e){
+            System.out.println("Query Failed: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public void querySongsMetaData(){
+        String sql = "SELECT * FROM " + TABLE_SONGS;
+        try (Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql)){
+            ResultSetMetaData meta = resultSet.getMetaData();
+            int numColumns = meta.getColumnCount();
+            for (int i = 1; i<= numColumns; i++){
+                System.out.format("Column %d in the songs table is names %s\n", i, meta.getColumnName(i));
+            }
+        } catch (SQLException e){
+            System.out.println("Could not get meta data for the songs table");
         }
     }
 }
