d87db1c3e3386bea3e675ba46599d78ee1d4871c
Chunyue Ma <cma01@wesleyan.edu>
2021-09-28 11:19:51 -0400

    pq demo #19

--- src/com/chunyue/collection/queue/PriorityQueueDemo.java
+++ src/com/chunyue/collection/queue/PriorityQueueDemo.java
@@ -1,7 +1,33 @@
 package com.chunyue.collection.queue;
 
-public class PriorityQueueDemo {
-    public static void main(String[] args) {
+import java.time.LocalDate;
+import java.util.PriorityQueue;
 
+public class PriorityQueueDemo {
+    private static PriorityQueue<LocalDate> dates = new PriorityQueue<>();
+    static {
+        dates.add(LocalDate.of(1998, 6, 19));
+        dates.add(LocalDate.of(1999, 4, 19));
+        dates.add(LocalDate.of(1973, 5, 16));
+        dates.add(LocalDate.of(1983, 7, 23));
+    }
+
+    public static void main(String[] args) {
+        // iteratingPQ();
+        // removingFromPQ();
+    }
+
+    public static void iteratingPQ(){
+        System.out.println("Iterating PQ does not give any fixed order...");
+        for (LocalDate date: dates){
+            System.out.println(date);
+        }
+    }
+
+    public static void removingFromPQ(){
+        System.out.println("Removing from PQ follows certain order based on the priority...");
+        while (!dates.isEmpty()){
+            System.out.println("Removing: " + dates.remove());
+        }
     }
 }
