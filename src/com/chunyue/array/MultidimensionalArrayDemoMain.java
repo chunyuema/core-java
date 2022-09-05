c824cabf39502bbd0af342c3d0e46e7d3343a9fb
Chunyue Ma <cma01@wesleyan.edu>
2022-09-05 15:41:29 -0700

    update typo and add functions for array initialization and printing

--- src/com/chunyue/array/MultidimensionalArrayDemoMain.java
+++ src/com/chunyue/array/MultidimensionalArrayDemoMain.java
@@ -5,13 +5,19 @@ import java.util.Arrays;
 public class MultidimensionalArrayDemoMain {
     public static void main(String[] args) {
         // multidimensionalArrayCreation();
-        // easyMutidimensionalArrayCreation();
-        shallowCopyOfArray();
+        // easyMultidimensionalArrayCreation();
+        // shallowCopyOfArray();
+        printingMultiDimensionalArray();
     }
 
     public static void multidimensionalArrayCreation(){
         int[][] arr = new int[3][3];
-        System.out.println("Initial multidimensional array: " + Arrays.toString(arr));
+        System.out.println("Initial multidimensional array: ");
+        System.out.println(Arrays.toString(arr));
+        for (int[] a: arr){
+            System.out.println(Arrays.toString(a));
+        }
+        System.out.println("--------");
         arr[0][0]=1;
         arr[0][1]=2;
         arr[0][2]=3;
@@ -21,16 +27,19 @@ public class MultidimensionalArrayDemoMain {
         arr[2][0]=7;
         arr[2][1]=8;
         arr[2][2]=9;
+        System.out.println("After initialization: ");
+        System.out.println(Arrays.toString(arr));
         for (int[] a: arr){
             System.out.println(Arrays.toString(a));
         }
     }
 
-    public static void easyMutidimensionalArrayCreation(){
+    public static void easyMultidimensionalArrayCreation(){
         int[][] arr = {{1, 2, 3},{3, 4, 6},{5, 7, 8}};
-        for (int i=0; i<arr.length; i++){
-            for (int j=0; j<arr[0].length; j++){
-                System.out.print(arr[i][j]+" ");
+        // using the enhanced for loop from below
+        for (int[] ints : arr) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(ints[j] + " ");
             }
             System.out.println();
         }
@@ -39,9 +48,14 @@ public class MultidimensionalArrayDemoMain {
     public static void shallowCopyOfArray(){
         int[][] a = {{1,3,4},{3,4,5}};
         System.out.println("original array: " + Arrays.toString(a));
-        // clone only produdes a shallow copy fo the original matrice
+        // clone only produces a shallow copy fo the original matrices
         int[][] b = a.clone();
         System.out.println("b and a are different: " + a + " | " + b);
         System.out.println("but b's content is the same as a's: " + Arrays.toString(b));
+    }
+
+    public static void printingMultiDimensionalArray(){
+        int[][][] threeDArray = {{{0, 1}, {1, 2}}, {{2, 3}, {3, 4}}};
+        System.out.println(Arrays.deepToString(threeDArray));
     }
 }
--- src/com/chunyue/array/SingleDimensionArrayDemoMain.java
+++ src/com/chunyue/array/SingleDimensionArrayDemoMain.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 public class SingleDimensionArrayDemoMain {
     public static void main(String[] args) {
         // arrayCreation();
-        // easyArrayDelcaration();
-        // forLoop();
+        // easyArrayDeclaration();
+        // loopThroughArray();
         // anonymousArray(new int[]{10, 22, 33, 44});
-        // className();
+        // classNameOfArray();
         // copyArray();
-        deepCloneOfArray();
+        // deepCloneOfArray();
     }
 
     public static void arrayCreation(){
@@ -22,12 +22,12 @@ public class SingleDimensionArrayDemoMain {
         System.out.println("With initialization a: " + Arrays.toString(a));
     }
 
-    public static void easyArrayDelcaration(){
+    public static void easyArrayDeclaration(){
         int[] ints = {1, 2, 34, 5, 67};
-        System.out.println("Easy initialziation of array: " + Arrays.toString(ints));
+        System.out.println("Easy initialization of array: " + Arrays.toString(ints));
     }
 
-    public static void forLoop(){
+    public static void loopThroughArray(){
         int[] ints = {3, 5, 67, 87, 43};
         for (int i: ints){
             System.out.println("Current integer is:  " + i);
@@ -38,24 +38,25 @@ public class SingleDimensionArrayDemoMain {
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void className(){
+    public static void classNameOfArray(){
         int[] arr = {4, 5, 6};
         System.out.println("class of an array: " + arr.getClass());
         System.out.println("class name of an array: " + arr.getClass().getName());
     }
 
     public static void copyArray(){
-        char[] orginalChars = {'a','b','c','d','r'};
+        char[] originalChars = {'a','b','c','d','r'};
         char[] newChars = new char[3];
         // use arraycopy provided by the System class
-        System.arraycopy(orginalChars, 1, newChars, 0, 3);
+        System.arraycopy(originalChars, 1, newChars, 0, 3);
         System.out.println(newChars);
+        System.out.println(Arrays.toString(newChars));
     }
 
     public static void deepCloneOfArray(){
         int[] arr = {1, 2, 3};
         System.out.println("original array: " + Arrays.toString(arr));
-        // clone method does deepcopy for the single dimension array
+        // clone method does deep copy for the single dimension array
         int[] clone = arr.clone();
         System.out.println("cloned array: " + Arrays.toString(clone));
         System.out.println("they are not equal: " + (arr==clone));
