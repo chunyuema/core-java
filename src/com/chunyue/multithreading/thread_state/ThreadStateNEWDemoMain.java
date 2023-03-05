6b7059098a87832f4450dd4c2e2fb6e4d573b226
Chunyue Ma <cma01@wesleyan.edu>
2023-03-05 11:34:26 -0800

    add demo code for thread in NEW state

---
+++ src/com/chunyue/multithreading/thread_state/ThreadStateNEWDemoMain.java
@@ -0,0 +1,16 @@
+package com.chunyue.multithreading.thread_state;
+
+public class ThreadStateNEWDemoMain {
+
+    private static void threadInNewState() {
+        Thread thread = new Thread(() -> {
+        });
+        System.out.println(
+                "A newly created thread has state: " +
+                        thread.getState());
+    }
+
+    public static void main(String[] args) {
+        threadInNewState();
+    }
+}
