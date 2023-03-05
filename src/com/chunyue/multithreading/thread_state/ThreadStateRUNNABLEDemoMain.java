d2c572ae81c8820af9f2ef0c8657dc975cba0f18
Chunyue Ma <cma01@wesleyan.edu>
2023-03-05 11:42:50 -0800

    add demo code for thread in RUNNABLE state

---
+++ src/com/chunyue/multithreading/thread_state/ThreadStateRUNNABLEDemoMain.java
@@ -0,0 +1,21 @@
+package com.chunyue.multithreading.thread_state;
+
+public class ThreadStateRUNNABLEDemoMain {
+
+    private static void threadInRUNNABLEState() {
+        Thread thread = new Thread(() -> {
+            System.out.println("Thread has started working...");
+            for (int i = 0; i < 10; i++) {
+                System.out.println(i);
+            }
+            System.out.println("Thread has finished working...");
+        });
+        System.out.println("A recently created thread has state: " + thread.getState());
+        thread.start();
+        System.out.println("A running thread has state: " + thread.getState());
+    }
+
+    public static void main(String[] args) {
+        threadInRUNNABLEState();
+    }
+}
