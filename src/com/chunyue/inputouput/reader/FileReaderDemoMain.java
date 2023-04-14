c02be5fd590b0e74e7461ef5205e9e4b06327a8e
Chunyue Ma <cma01@wesleyan.edu>
2023-04-13 21:01:54 -0700

    Read into char buffer with file reader #15

--- src/com/chunyue/inputouput/reader/FileReaderDemoMain.java
+++ src/com/chunyue/inputouput/reader/FileReaderDemoMain.java
@@ -10,42 +10,64 @@ public class FileReaderDemoMain {
     public static void main(String[] args) {
         // basicFileReader();
         // fileReaderTryWithResource();
+        readIntoCharBuffer();
     }
 
-    public static void basicFileReader(){
+    /*
+     * Compare this with the output from FileInputStream, because we are reading
+     * char instead of reading buffer, the last char 马 can be read
+     */
+    public static void readIntoCharBuffer() {
+        try {
+            FileReader fr = new FileReader("./test.txt");
+            char[] charBuffer = new char[10];
+            int i = fr.read(charBuffer);
+            System.out.println("Number of char read: " + i);
+            for (char c : charBuffer) {
+                System.out.println(c);
+            }
+            fr.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void basicFileReader() {
         Scanner scanner = null;
         Map<Integer, String> res = new HashMap<>();
         try {
             scanner = new Scanner(new FileReader("./text/file.txt"));
             scanner.useDelimiter(",");
-            while (scanner.hasNextLine()){
+            while (scanner.hasNextLine()) {
                 int num = scanner.nextInt();
                 scanner.skip(scanner.delimiter());
                 String description = scanner.nextLine();
                 res.put(num, description);
             }
             System.out.println(res);
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            // scanner.close will close the FileReader as well since it is an instance of Closable
-            if (scanner != null) scanner.close();
+            // scanner.close will close the FileReader as well since it is an instance of
+            // Closable
+            if (scanner != null)
+                scanner.close();
         }
     }
 
     // simplify the above code using try with resources
-    public static void fileReaderTryWithResource(){
+    public static void fileReaderTryWithResource() {
         Map<Integer, String> res = new HashMap<>();
-        try (Scanner scanner = new Scanner(new FileReader("./text/file.txt"))){
+        try (Scanner scanner = new Scanner(new FileReader("./text/file.txt"))) {
             scanner.useDelimiter(",");
-            while (scanner.hasNextLine()){
+            while (scanner.hasNextLine()) {
                 int num = scanner.nextInt();
                 scanner.skip(scanner.delimiter());
                 String description = scanner.nextLine();
                 res.put(num, description);
             }
             System.out.println(res);
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
