2633f29ad67c50ea9dd1b8076c8ef5cab2cea6e9
Chunyue Ma <cma01@wesleyan.edu>
2023-04-15 09:49:27 -0700

    Add close in finally to release resource #15

--- src/com/chunyue/inputouput/output/FileOutputStreamDemoMain.java
+++ src/com/chunyue/inputouput/output/FileOutputStreamDemoMain.java
@@ -1,6 +1,9 @@
 package com.chunyue.inputouput.output;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.xml.catalog.Catalog;
 
 public class FileOutputStreamDemoMain {
     public static void main(String[] args) {
@@ -8,27 +11,45 @@ public class FileOutputStreamDemoMain {
         writeString();
     }
 
-    public static void writeByte(){
+    public static void writeByte() {
+        FileOutputStream fout = null;
         try {
-            FileOutputStream fout = new FileOutputStream("./test1.txt");
+            fout = new FileOutputStream("./test1.txt");
             fout.write(65);
             fout.close();
             System.out.println("Successfully written...");
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
+        } finally {
+            if (fout != null) {
+                try {
+                    fout.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
-    public static void writeString(){
+    public static void writeString() {
+        FileOutputStream fout = null;
         try {
-            FileOutputStream fout = new FileOutputStream("./test.txt");
+            fout = new FileOutputStream("./test.txt");
             String s = "Welcome to core java repo";
             byte[] bytes = s.getBytes();
             fout.write(bytes);
             fout.close();
             System.out.println("Successfully written....");
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
+        } finally {
+            if (fout != null) {
+                try {
+                    fout.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
