589f0e4dd5dfe1973c995e1b84488f1fff48485f
Chunyue Ma <cma01@wesleyan.edu>
2023-12-08 07:23:20 -0800

    Get all constructors

--- src/com/chunyue/reflect/ReflectDemo.java
+++ src/com/chunyue/reflect/ReflectDemo.java
@@ -1,5 +1,7 @@
 package com.chunyue.reflect;
 
+import java.lang.reflect.Constructor;
+
 public class ReflectDemo {
 
     private static void getClassDemo() throws Exception {
@@ -13,14 +15,31 @@ public class ReflectDemo {
         System.out.println(clazz3);
     }
 
+    private static void getAllConstructors() {
+        Class clazz = Dog.class;
+        // This will only get public constructors
+        for (Constructor c : clazz.getConstructors()) {
+            System.out.println(c.getName() + " takes " + c.getParameterCount() + " arguments.");
+        }
+
+        // This will get all constructors
+        for (Constructor c : clazz.getDeclaredConstructors()) {
+            System.out.println(c.getName() + " takes " + c.getParameterCount() + " arguments.");
+        }
+    }
+
     private static void getConstructorThroughReflect() throws Exception {
         Class clazz = Class.forName("com.chunyue.reflect.Dog");
         Dog dog = (Dog) clazz.getDeclaredConstructor().newInstance();
         System.out.println(dog);
+
+        Dog dog2 = (Dog) clazz.getConstructor().newInstance();
+        System.out.println(dog2);
     }
 
     public static void main(String[] args) throws Exception {
         // getClassDemo();
-        getConstructorThroughReflect();
+        // getConstructorThroughReflect();
+        getAllConstructors();
     }
 }
